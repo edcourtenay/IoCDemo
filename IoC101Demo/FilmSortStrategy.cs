ca56106047a80b72c85bb08087e3cd6475a0b75a
Ed Courtenay <ed@edcourtenay.co.uk>
2015-03-24 15:46:49 +0000

    Add FilmSortStrategy

---
+++ IoC101Demo/FilmSortStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IoC101Demo
+{
+    internal class FilmSortStrategy : IFilmSortStrategy
+    {
+        public Func<IFilm, int> OrderFunction
+        {
+            get { return film => film.Year; }
+        }
+    }
+}
